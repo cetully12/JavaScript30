d7c4a44d563e9b3414bc231dc59f02c25be8dcb2
Colleen <93383787+cetully12@users.noreply.github.com>
2023-02-13 17:31:14 -0500

    Wrote psuedo code for JS

---
+++ 03 - CSS Variables/js/main.js
@@ -0,0 +1,7 @@
+// Adjust border color around photo when Spacing metric adjusted
+
+// Blur photo when Blur metric adjusted
+
+// Update border color when Base Color changed
+
+const inputs = document.querySelectorAll('.controls input');
